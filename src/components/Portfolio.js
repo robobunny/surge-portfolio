d9f4498150e5df1c98df0c56670c4eea65e06db8
William Duraney <william.duraney@gmail.com>
2020-08-04 21:16:02 -0400

    added deploy script and fixed link typo

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -6,7 +6,8 @@ const Portfolio = () => {
       <h1>My Portfolio</h1>
       <p>I built this portfolio site using React with React Router. The styles are done using SCSS.</p>
       <p>The site has a "mobile first" design. That means that the styles are written for viewing on a phone and media queries are used to adjust the styles for viewing on larger screens. It's generally easier to design the site to look good on mobile and then adjust for a large screen than vice-versa. Plus your phone won't be tasked with loading all the styles for a big screen.</p>
-      <p><a href="https://github.com/robobunny/">Check out the source code</a></p>
+      <p>The way the main nav bar changes on the phone uses the <code>useState</code> hook. Check it out by turning on phone mode in your browser's dev tools. (or by resizing your browser window!)</p>
+      <p><a href="https://github.com/robobunny/portfolio">Check out the source code</a></p>
     </div>
   )
 }
