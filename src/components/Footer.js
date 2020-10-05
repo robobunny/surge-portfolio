f3aaf5a02093eb0f2a3ca30644f7671a340b4e20
William Duraney <william.duraney@gmail.com>
2020-10-05 15:12:25 -0400

    make links in footer clickable

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,10 +3,12 @@ import React from "react";
 const Footer = () => (
   <footer>
     <div className="footer container">
-      <div className="footer-info">
-        <p>Github: www.github.com/robobunny</p>
-        <p>LinkedIn: www.linkedin.com/in/william-duraney</p>
-        <p>Twitter: @_robobunny</p>
+    <div className="footer-info">
+        <p>Github: <a href="https://www.github.com/robobunny">www.github.com/robobunny</a></p>
+        <p>LinkedIn: <a href="https://www.linkedin.com/in/william-duraney">www.linkedin.com/in/william-duraney</a></p>
+        <p>Twitter: <a href="https://www.twitter.com/_robobunny">@_robobunny</a></p>
+        <p>Does anybody really ever read all the fine print at the bottom of a website like this?</p>
+        <p>I sure dont.</p>
       </div>
     </div>
   </footer>
